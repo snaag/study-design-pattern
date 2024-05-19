d2ed457971349d9e37f7c915701ad0af36291238
snaag <snaag.dev@gmail.com>
2024-05-19 20:45:24 +0900

    [Proxy] before 코드 구현

--- src/main/java/designpattern/study/design/pattern/demo/proxy/before/CoffeeMachineService.java
+++ src/main/java/designpattern/study/design/pattern/demo/proxy/before/CoffeeMachineService.java
@@ -1,27 +1,15 @@
 package designpattern.study.design.pattern.demo.proxy.before;
 
 public class CoffeeMachineService {
-    CoffeeMachineTimer timer;
-
-    public CoffeeMachineService(CoffeeMachineTimer timer) {
-        this.timer = timer;
-    }
-
     public void makeEspresso() {
-        timer.start();
         System.out.println("에스프레소를 만들었습니다.");
-        timer.end();
     }
 
     public void makeAmericano() {
-        timer.start();
         System.out.println("아메리카노를 만들었습니다.");
-        timer.end();
     }
 
     public void makeLatte() {
-        timer.start();
         System.out.println("라떼를 만들었습니다.");
-        timer.end();
     }
 }
--- src/main/java/designpattern/study/design/pattern/demo/proxy/before/ProxyClient.java
+++ src/main/java/designpattern/study/design/pattern/demo/proxy/before/ProxyClient.java
@@ -1,10 +1,35 @@
 package designpattern.study.design.pattern.demo.proxy.before;
 
 public class ProxyClient {
-    CoffeeMachineService service = new CoffeeMachineService(new CoffeeMachineTimer());
+    CoffeeMachineService service;
+    CoffeeMachineTimer timer;
+
+    public ProxyClient(CoffeeMachineService service, CoffeeMachineTimer timer) {
+        this.service = service;
+        this.timer = timer;
+    }
+
     public void run() {
         service.makeEspresso();
         service.makeAmericano();
         service.makeLatte();
+    }
+
+    public void makeEspresso() {
+        timer.start();
+        service.makeEspresso();
+        timer.end();
+    }
+
+    public void makeAmericano() {
+        timer.start();
+        service.makeAmericano();
+        timer.end();
+    }
+
+    public void makeLatte() {
+        timer.start();
+        service.makeLatte();
+        timer.end();
     }
 }
